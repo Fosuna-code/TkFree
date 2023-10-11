be69d3b243c8c97ed49606216d464adcfc594131
Fernando Osuna Manzo <ferosuna2001@gmail.com>
2023-10-10 18:19:34 -0700

    removing unnecessary files from repository

--- .dfx/local/canisters/backend/service.did.d.ts
+++
@@ -1,8 +0,0 @@
-import type { Principal } from '@dfinity/principal';
-import type { ActorMethod } from '@dfinity/agent';
-
-export interface _SERVICE {
-  'add' : ActorMethod<[bigint], bigint>,
-  'get' : ActorMethod<[], bigint>,
-  'inc' : ActorMethod<[], bigint>,
-}
